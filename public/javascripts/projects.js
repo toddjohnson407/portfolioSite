85ce93253d3529550492a8b8c6c314b05c3c6ee2
Todd Johnson <toddjohnson407@gmail.com>
2019-02-06 05:52:46 -0700

    webkit animation implementations

--- public/javascripts/projects.js
+++ public/javascripts/projects.js
@@ -1,5 +1,7 @@
 // @toddjohnson - projects page
 
+'use-strict';
+
 $('.card').click(function(){
   $(this).toggleClass('flipped');
 });
@@ -8,19 +10,13 @@ function sleep(ms) {
   return new Promise(resolve => setTimeout(resolve, ms));
 }
 
-var activateProjects = function() {
-  // project.className = 'container-animation';
-  projects.style = 'display: block; margin-top: 20px';
-  projectTitle.style = 'padding-top: 20px';
-  projectHead.style = 'margin-bottom: -8vh !important;'
-  projectHead.style = 'background-color: black; color: white; padding: 1vh 2vh 1vh 2vh';
-  // projectBack.className = 'animation-active';
+var activateProjects = async function() {
+  await sleep(1000);
+
+  projects.style = '';
 }
 
-var project = document.getElementById('project-container')
 var projects = document.getElementById('projects');
-var projectTitle = document.getElementById('projects-title');
-var projectHead = document.getElementById('projects-head');
-var projectBack = document.getElementById('projects-title-back');
 
-activateProjects();
+window.onload = function() { activateProjects() };
+// activateProjects();
